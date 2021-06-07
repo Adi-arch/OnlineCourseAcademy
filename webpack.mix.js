a58a61b6b82c8f193ee5c602c4ebc483d99b211a
Adi-arch <mca.1933@unigoa.ac.in>
2021-06-07 09:21:20 +0530

    assets of laravel/ui

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,19 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ]);
-mix.js('resources/js/theme.js', 'public/js');
-mix.postCss('resources/css/bootstrap.min.css', '/public/css');
-mix.postCss('resources/css/idash.css', '/public/css');
-if (mix.inProduction()) {
-    mix.version();
-}
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
