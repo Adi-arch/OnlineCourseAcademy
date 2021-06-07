0f7fe03b1abeb43a3f6522e0fb18f7c878a721b9
Adi-arch <mca.1933@unigoa.ac.in>
2021-06-07 09:22:44 +0530

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\InstructorDashboardController;
 // use App\Http\Controllers\CourseCartController;
-use App\Http\Controllers\InstaDashboard;
-use App\Http\Controllers\InstructorController;
+// use App\Http\Controllers\InstaDashboard;
+// use App\Http\Controllers\InstructorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,34 +21,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/', [PagesController::class,'index']);
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
-Route::get('/courseCreation', [InstructorController::class,'index'])->name('courseCreation');
-Route::get('/InstaDashboard', [InstaDashboard::class,'index'])->name('idashboard');
+Auth::routes();
 
-Route::group(['middleware' => 'auth'], function() {
-    Route::group(['middleware' => 'role:user', 'prefix' => 'user', 'as' => 'user.'], function() {
-        Route::resource('lessons', \App\Http\Controllers\Users\LessonController::class);
-    });
-    Route::group(['middleware' => 'role:user', 'prefix' => 'user', 'as' => 'user.'], function() {
-        Route::resource('shop', \App\Http\Controllers\Users\CourseCartController::class);
-    });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-   Route::group(['middleware' => 'role:instructor', 'prefix' => 'instructor', 'as' => 'instructor.'], function() {
-       Route::resource('courses', \App\Http\Controllers\Instructors\CourseController::class);
-   });
-    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
-        Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
-    });
-});
-
-// Route::get('/demoshop',[CourseCartController::class, 'shop'])->name('dshop');
-// Route::get('/cart', [CourseCartController::class, 'index'])->name('cart');
- Route::post('/add', [CourseCartController::class, 'store'])->name('addtoc');
-// Route::post('/update', [CourseCartController::class,'update'])->name('update');
-// Route::post('/remove', [CourseCartController::class,'remove'])->name('remove');
-// Route::post('/clear', [CourseCartController::class,'clear'])->name('clear');
 
