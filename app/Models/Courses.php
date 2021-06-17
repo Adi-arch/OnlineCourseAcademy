8ae4a7b7b7f01dcd87eb273319caeb8e4aa7169c
Adi-arch <mca.1933@unigoa.ac.in>
2021-06-17 08:50:36 +0530

    models for courses, enrollment

--- app/Models/Courses.php
+++ app/Models/Courses.php
@@ -9,12 +9,11 @@ class Courses extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = "cid";
     protected $fillable = ['cname','cprice','description','video_path','image_path','instructor_id'];
 
     public function instructor()
     {
-        return $this->belongsTo('App\Models\Instructor','instructor_id','id');
+        return $this->belongsTo(Instructor::class,'instructor_id','id');
     }
 
 
