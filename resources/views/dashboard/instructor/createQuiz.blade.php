b7007877f5cd5396cfda6ea54d24eca0c56dd511
1935-BhaktiNaik <mca.1935@unigoa.ac.in>
2021-07-05 16:13:58 +0530

    updated quiz creation blade

--- resources/views/dashboard/instructor/createQuiz.blade.php
+++ resources/views/dashboard/instructor/createQuiz.blade.php
@@ -2,33 +2,43 @@
 
 @section('content')
 <div class="container createQuiz">
-<hr class="mt-5">
-<div class="card mx-auto d-flex justify-content-center col-6">
-<form method="POST" action="{{route('instructor.uploadQuiz')}}">
-	{{ csrf_field() }}
+	<hr class="mt-5">
+	<div class="card mx-auto d-flex justify-content-center col-6">
+		<form method="POST" action="{{route('instructor.uploadQuiz')}}">
+			{{ csrf_field() }}
 
-	
-	  
-	  <div class="form-group">
-	    <label class="col-form-label" for="formGroupExampleInput2">Course Name</label>
-	    <input type="text" name="course" class="form-control" id="formGroupExampleInput2" placeholder="example:SWE111" required>
-	  </div>
-	  <div class="form-group">
-	    <label class="col-form-label" for="formGroupExampleInput2">Number Of Question</label>
-	    <input type="text" name="question_length" class="form-control" id="formGroupExampleInput2" placeholder="E.g 10" required>
-	  </div>
-	  <div class="form-group">
-	    <label class="col-form-label" for="formGroupExampleInput2">Set time</label>
-	    <input type="text" name="time" class="form-control" id="formGroupExampleInput2" placeholder="Enter In Minutes" required>
-	  </div>
-	  <div class="form-group">
-	    <input type="hidden" value="{{substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 5)), 0, 5)}}" name="uniqueid" class="form-control" id="formGroupExampleInput2">
-	  </div>
-	  <button type="Submit" class="btn btn-success btn-block">Submit</button>
-	
 
-</form>
-</div>
+
+			<div class="form-group">
+				 <label class="col-form-label" for="formGroupExampleInput2">Course Name</label> 
+				{{-- <input type="text" name="course" class="form-control" id="formGroupExampleInput2" placeholder="example:SWE111" required> --}}
+				<select name="course_id" class="form-control">
+					@foreach ($courses as $course)
+					<option name="course_id" value="{{$course->id}}">{{$course->cname}}</option>
+					@endforeach
+
+				</select>
+			</div>
+			<div class="form-group">
+				<label class="col-form-label" for="formGroupExampleInput2">Number Of Question</label>
+				<input type="text" name="question_length" class="form-control" id="formGroupExampleInput2"
+					placeholder="E.g 10" required>
+			</div>
+			<div class="form-group">
+				<label class="col-form-label" for="formGroupExampleInput2">Set time</label>
+				<input type="text" name="time" class="form-control" id="formGroupExampleInput2"
+					placeholder="Enter In Minutes" required>
+			</div>
+			<div class="form-group">
+				<input type="hidden"
+					value="{{substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 5)), 0, 5)}}"
+					name="uniqueid" class="form-control" id="formGroupExampleInput2">
+			</div>
+			<button type="Submit" class="btn btn-success btn-block">Submit</button>
+
+
+		</form>
+	</div>
 </div>
 
 @endsection
