b8d66a37659331c878e0b76b065099db4b2b090f
kingofcars3711 <kingofcars3711@gmail.com>
2021-07-07 10:40:02 +0530

    user quiz model & table created

---
+++ database/migrations/2021_07_06_171132_create_user_quizzes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_quizzes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('uniqueId');
+            $table->integer('score')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_quizzes');
+    }
+}
